cabaf3957ed7ae21b717d7b7f446c96c2c8e9e01
akashpk41 <akashpk84100@gmail.com>
2025-11-06 18:01:44 +0600

    auth routes controller and others folder structer created

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
-const express = require("express");
-require("dotenv").config()
-
+import "dotenv/config";
+import express from "express";
+import authRoute from "./router/auth.route.js";
 // configure
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// routes
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server Is Running🔥 ${PORT} `);
