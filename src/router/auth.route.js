c3bbbb5abd605ea47289872507d25fac940f1eba
akashpk41 <akashpk84100@gmail.com>
2025-11-06 23:10:47 +0600

    C:/Program Files (x86)/Git/me routes created for check logged user info

--- src/router/auth.route.js
+++ src/router/auth.route.js
@@ -9,11 +9,16 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.use(protectRoute)
+router.use(protectRoute);
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/onboarding", onBoard);
+
+// check if user is logged in ;
+router.get("/me", (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 export default router;
