9898d611dfcbe67a099804d46894774bfdbad89e
akashpk41 <akashpk84100@gmail.com>
2025-11-06 20:47:00 +0600

    signup functionality complete and everything working perfectly

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,9 +1,74 @@
-export const signup = async(req,res) => {
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-}
-export const login = async(req,res) => {
+export const signup = async (req, res) => {
+  const { email, password, fullName } = req.body;
 
-}
-export const logout = async(req,res) => {
-     res.send('iam logout')
-}
+  try {
+    // check all fields
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    //     check password length
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    //     check correct email syntax
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    //     check if user already exist in the database
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists, please use a different one" });
+    }
+
+    //     generate random profile pic for user
+    const idx = Math.floor(Math.random() * 100) + 1; // generate a num between 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    //     save the user to the database
+    const newUser = await User.create({
+      email,
+      fullName,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    //     TODO : Create stream user later
+
+    //    add  json web token
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {};
+export const logout = async (req, res) => {
+  res.send("iam logout");
+};
--- src/server.js
+++ src/server.js
@@ -1,15 +1,29 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import authRoute from "./router/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import authRoute from "./router/auth.route.js";
 // configure
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// middlewares;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
 
 // routes
 app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Server Is Running🔥 ${PORT} `);
-  connectDB()
+  connectDB();
 });
