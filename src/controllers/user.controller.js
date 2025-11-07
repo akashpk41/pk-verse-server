f6596eff0ad4ed445c402827e61eb459a428004c
akashpk41 <akashpk84100@gmail.com>
2025-11-07 11:29:35 +0600

    recommended user functionality created

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,21 @@
-export const getRecommendedUsers = async(req,res) => {
+import User from "../models/User";
 
-}
-export const getMyFriends = async(req,res) => {
+export const getRecommendedUsers = async (req, res) => {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = await User.findById(currentUserId);
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } }, // in recommended uses list dont show the current logged in user and his friends
+        { $id: { $nin: currentUser.friends } },
+        { isOnboarded: true },
+      ],
+    });
 
-}
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.error("Error in getRecommendedUsers controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const getMyFriends = async (req, res) => {};
